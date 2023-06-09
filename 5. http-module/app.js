b6bcc5b8de5adb79ac4b475e11643fd21e76c578
HordesOfGhost <85671929+HordesOfGhost@users.noreply.github.com>
2023-06-09 10:00:03 +0545

    Complete the 1 hour course

---
+++ 5. http-module/app.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url == '/'){
+        res.write('Hello World');
+        res.end();
+    }
+    if (req.url == '/api/courses'){
+        res.write(JSON.stringify([1,2,3]));
+        res.end();
+    }
+});
+
+// server.on('connection', (socket) => {
+//     console.log('New Connection...');
+// });
+
+server.listen(3000);
+ 
+console.log('Listening on port 3000...');
